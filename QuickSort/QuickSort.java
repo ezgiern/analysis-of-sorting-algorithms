25f33388472cd20856be1f99af41a17808675cbb
Ezgi Eren <84743502+ezgiern@users.noreply.github.com>
2024-07-02 15:53:22 +0300

    Add files via upload

--- QuickSort/QuickSort.java
+++ QuickSort/QuickSort.java
@@ -1,1 +1,37 @@
+import java.lang.Math;
+public class QuickSort {
 
+    public static int findNthElement(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+        return arr[(int) Math.ceil(arr.length / 2.0) - 1];
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low + 1;
+        for (int j = low + 1; j <= high; j++) {
+            if (arr[j] < pivot) {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr, low, i - 1);
+        return i - 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+}
