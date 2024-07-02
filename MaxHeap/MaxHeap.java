00473e4990dec95e30b187ee9b01403211af96f6
Ezgi Eren <84743502+ezgiern@users.noreply.github.com>
2024-07-02 15:50:02 +0300

    Add files via upload

--- MaxHeap/MaxHeap.java
+++ MaxHeap/MaxHeap.java
@@ -1,1 +1,16 @@
+import java.util.PriorityQueue;
 
+public class MaxHeap {
+
+    public static int findNthElement(int[] arr) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for (int num : arr) {
+            maxHeap.offer(num);
+        }
+        for (int i = 0; i < arr.length / 2; i++) {
+            maxHeap.poll();
+        }
+        return maxHeap.peek();
+    }
+
+}
