8b5c04274ce5441531ad3a8473078facba626fcf
Ezgi Eren <84743502+ezgiern@users.noreply.github.com>
2024-07-02 15:48:12 +0300

    Add files via upload

--- InsertionSort/InsertionSort.java
+++ InsertionSort/InsertionSort.java
@@ -1,1 +1,31 @@
+import java.lang.Math;
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = temp;
+        }
+
+    }
+    public static int findMiddleElement(int[] arr) {
+        // Sort the array
+        insertionSort(arr);
+        
+        return arr[(int) Math.ceil(arr.length / 2.0) - 1]; // Subtract 1 to arrange to array indexing
+    }
+
+}
+
+
+
 
